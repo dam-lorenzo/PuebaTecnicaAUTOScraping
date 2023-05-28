aade4cba14c335d81ffbf2b5250252f361b9f363
damian <dam.lorenzo@hotmail.com>
2023-05-28 01:16:12 -0300

    Update url.py: add pagination for products

--- utils/url.py
+++ utils/url.py
@@ -3,8 +3,10 @@
 class URL():
     BASE        = 'https://www.hiperlibertad.com.ar/'
     CATEGORIES  = 'https://www.hiperlibertad.com.ar/api/catalog_system/pub/category/tree/50'
-    PRODUCTS    = 'https://www.hiperlibertad.com.ar/api/catalog_system/pub/products/search/{slug}?O=OrderByTopSaleDESC&_from=0&_to=23&ft&sc=1'
+    PRODUCTS    = 'https://www.hiperlibertad.com.ar/api/catalog_system/pub/products/search/{slug}?O=OrderByTopSaleDESC&_from={_from}&_to={_to}&ft&sc=1'
+    steps       = 23
 
     @classmethod
-    def get_products_url(cls, slug):
-        return cls.PRODUCTS.format(slug=slug)
+    def get_products_url(cls, slug, _from):
+        _to = _from + cls.steps
+        return cls.PRODUCTS.format(slug=slug, _from=_from, _to=_to)
