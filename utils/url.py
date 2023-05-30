0249c8eca14fd5687037cdd816029a6c1bef57a5
damian <dam.lorenzo@hotmail.com>
2023-05-29 23:48:18 -0300

    Update url.py: add stores url and add stores navigation to products endpoint

--- utils/url.py
+++ utils/url.py
@@ -3,10 +3,11 @@
 class URL():
     BASE        = 'https://www.hiperlibertad.com.ar/'
     CATEGORIES  = 'https://www.hiperlibertad.com.ar/api/catalog_system/pub/category/tree/50'
-    PRODUCTS    = 'https://www.hiperlibertad.com.ar/api/catalog_system/pub/products/search/{slug}?O=OrderByTopSaleDESC&_from={_from}&_to={_to}&ft&sc=1'
+    PRODUCTS    = 'https://www.hiperlibertad.com.ar/api/catalog_system/pub/products/search/{slug}?O=OrderByTopSaleDESC&_from={_from}&_to={_to}&ft&sc={store}'
+    STORES    = 'https://www.hiperlibertad.com.ar/institucional/sucursales'
     steps       = 23
 
     @classmethod
-    def get_products_url(cls, slug, _from):
+    def get_products_url(cls, slug, _from, store):
         _to = _from + cls.steps
-        return cls.PRODUCTS.format(slug=slug, _from=_from, _to=_to)
+        return cls.PRODUCTS.format(slug=slug, _from=_from, _to=_to, store=store)
