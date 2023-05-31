e2d888bc5a9798664f0c26a147a69a53454a5791
damian <dam.lorenzo@hotmail.com>
2023-05-30 23:58:02 -0300

    Update main.py: add argument parser

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from crawler.crawler import Crawler
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--storeName', type=str, help='Store name to scrap', default=None)
+    parser.add_argument('--storeIndex', type=str, help='Store index to scrap', default=None)
+    parser.add_argument('--input', type=bool, help='If you want to enter the store manually', nargs='?', const=True, default=False)
     args = parser.parse_args()
-    crawler = Crawler()
+    store_name = args.storeName
+    store_index = args.storeIndex
+    use_input = args.input
+    crawler = Crawler(store_name=store_name, store_index=store_index, use_input=use_input)
     crawler.run()
